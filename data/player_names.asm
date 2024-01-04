046e122e34288c10503a6ff1bfd32e2dd733a367
GameDevMadeEasy <jglover88@hotmail.com>
2024-01-04 00:19:59 -0600

    Groudon implemented. Changed default names to Scott, Steve and Speed. Text speed is fast, Battle Animation is Off and Battle Style is Set by default.

---
+++ data/player_names.asm
@@ -0,0 +1,31 @@
+IF DEF(_RED)
+DefaultNamesPlayer:
+	db   "NEW NAME"
+	next "STEVE"
+	next "SPEED"
+	next "SCOTT"
+	db   "@"
+
+DefaultNamesRival:
+	db   "NEW NAME"
+	next "SPEED"
+	next "SCOTT"
+	next "STEVE"
+	db   "@"
+ENDC
+
+IF DEF(_BLUE)
+DefaultNamesPlayer:
+	db   "NEW NAME"
+	next "STEVE"
+	next "SPEED"
+	next "SCOTT"
+	db   "@"
+
+DefaultNamesRival:
+	db   "NEW NAME"
+	next "SPEED"
+	next "SCOTT"
+	next "STEVE"
+	db   "@"
+ENDC
---
+++ data/player_names_list.asm
@@ -0,0 +1,27 @@
+IF DEF(_RED)
+DefaultNamesPlayerList:
+	db "NEW NAME@"
+	db "STEVE@"
+	db "SPEED@"
+	db "SCOTT@"
+
+DefaultNamesRivalList:
+	db "NEW NAME@"
+	db "STEVE@"
+	db "SCOTT@"
+	db "SPEED@"
+ENDC
+
+IF DEF(_BLUE)
+DefaultNamesPlayerList:
+	db "NEW NAME@"
+	db "STEVE@"
+	db "SPEED@"
+	db "SCOTT@"
+
+DefaultNamesRivalList:
+	db "NEW NAME@"
+	db "STEVE@"
+	db "SCOTT@"
+	db "SPEED@"
+ENDC
